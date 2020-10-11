7cdd4bc927748df214935b963fdcb8771d6be431
Polina Fedorova <ps.fedorova@yandex.ru>
2020-10-11 17:13:51 +0500

    Updates

---
+++ precache-manifest.2973bec592791f15081646de0f3f14d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be60c6c5ebd1619b2129d1af2956eee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0b23e3dcc8c5910849cc",
+    "url": "./static/css/main.f07f37be.chunk.css"
+  },
+  {
+    "revision": "75e40ffed8728ce1e787",
+    "url": "./static/js/2.d5e2f51d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d5e2f51d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b23e3dcc8c5910849cc",
+    "url": "./static/js/main.cc46f2c7.chunk.js"
+  },
+  {
+    "revision": "6fdb30c7fff8025b64e8",
+    "url": "./static/js/runtime-main.5e8e3037.js"
+  }
+]);
